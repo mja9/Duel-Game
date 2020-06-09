224e1bff2315e1740a39f50ea4fd080ca9a98ea2
mja9 <mja9@rice.edu>
2020-06-08 23:57:51 -0500

    model and view connected and initializing normally

--- Dastardly Duel/src/view/GameGUI.java
+++ Dastardly Duel/src/view/GameGUI.java
@@ -14,40 +14,25 @@ public class GameGUI extends JFrame {
 	private JPanel contentPane;
 
 	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					GameGUI frame = new GameGUI();
-					frame.setVisible(true);
-					frame.initGUI();
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
-
-	/**
 	 * Create the frame.
 	 */
 	public GameGUI() {
+		initGUI();
+	}
+	
+	public void initGUI() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(0, 0, 0, 0));
 		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
-	}
-	
-	public void initGUI() {
+		
 		contentPane.add(new DemoLevel());
 	}
 	
 	public void start() {
-		
+		setVisible(true);
 	}
 	
 	public void update() {
