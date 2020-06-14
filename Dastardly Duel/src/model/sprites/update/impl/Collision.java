eb500b15fda378217ddfc00fec2befe542639c96
mja9 <mja9@rice.edu>
2020-06-13 22:08:13 -0500

    ground work laid out for observer-observer interactions for platforms

---
+++ Dastardly Duel/src/model/sprites/update/impl/Collision.java
@@ -0,0 +1,16 @@
+package model.sprites.update.impl;
+
+import model.sprites.ASprite;
+import model.sprites.update.IUpdateStrategy;
+
+public class Collision implements IUpdateStrategy {
+
+	@Override
+	public void init() {		
+	}
+
+	@Override
+	public void updateState(ASprite context) {		
+	}
+
+}
