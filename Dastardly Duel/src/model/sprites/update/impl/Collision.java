31409d5ee9e0dd3a53982197091566d7a981b5dc
mja9 <mja9@rice.edu>
2020-06-15 16:15:45 -0500

    initial implementation of collision detection

--- Dastardly Duel/src/model/sprites/update/impl/Collision.java
+++ Dastardly Duel/src/model/sprites/update/impl/Collision.java
@@ -1,5 +1,7 @@
 package model.sprites.update.impl;
 
+import java.awt.Point;
+
 import model.sprites.ASprite;
 import model.sprites.update.IUpdateStrategy;
 import util.dispatcher.ICommand;
@@ -13,11 +15,35 @@ public class Collision implements IUpdateStrategy {
 
 	@Override
 	public void updateState(ASprite thisContext, IDispatcher<ICommand> dispatcher) {
+		
+		Point thisPosition = thisContext.getPosition();
+		
 		dispatcher.sendMessage(new ICommand() {
 
 			@Override
-			public void apply(ASprite nextContext, IDispatcher<ICommand> dispatcher) {
-				
+			public void apply(ASprite otherContext, IDispatcher<ICommand> dispatcher) {
+				if (otherContext != thisContext) {
+					
+					Point othersPos = otherContext.getPosition();
+					
+					// Check x-position -- check left side
+					if (((othersPos.x - otherContext.getWidth() <= thisPosition.x - thisContext.getWidth())
+							| (othersPos.x - otherContext.getWidth() <= thisPosition.x + thisContext.getWidth()))
+							// Check right side
+							| ((othersPos.x + otherContext.getWidth() <= thisPosition.x - thisContext.getWidth())
+							| (othersPos.x + otherContext.getWidth() <= thisPosition.x + thisContext.getWidth()))) {
+						
+						// Check y-position -- check top side
+						if (((othersPos.y - otherContext.getHeight() <= thisPosition.y - thisContext.getHeight())
+								| (othersPos.y - otherContext.getHeight() <= thisPosition.y + thisContext.getHeight()))
+								// Check bottom side
+								| ((othersPos.y + otherContext.getHeight() <= thisPosition.y - thisContext.getHeight())
+								| (othersPos.y + otherContext.getHeight() <= thisPosition.y + thisContext.getHeight()))) {
+							
+						}
+						
+					}
+				}
 			}
 			
 		});
