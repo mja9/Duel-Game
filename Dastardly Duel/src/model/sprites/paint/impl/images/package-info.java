c6856509263b18aaf0b3f572c6e11388dc435b93
mja9 <mja9@rice.edu>
2020-06-16 16:58:00 -0500

    added reference for image assets source

---
+++ Dastardly Duel/src/model/sprites/paint/impl/images/package-info.java
@@ -0,0 +1,7 @@
+package model.sprites.paint.impl.images;
+
+/**
+ * This package holds the image assets used as skins for the 
+ * player. These images were created by and are property of 
+ * Andressa Lemgruber Viol.
+ */
