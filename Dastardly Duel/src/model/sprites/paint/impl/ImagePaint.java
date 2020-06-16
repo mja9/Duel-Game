3ef275142cae6128d65432fad38ca3ab9a6c8cb1
mja9 <mja9@rice.edu>
2020-06-16 17:28:32 -0500

    implemented init of image paint strategy

--- Dastardly Duel/src/model/sprites/paint/impl/ImagePaint.java
+++ Dastardly Duel/src/model/sprites/paint/impl/ImagePaint.java
@@ -12,8 +12,9 @@ import model.sprites.ASprite;
 public class ImagePaint extends APaintStrategy {
 	
 	private ImageObserver _canvas;
-	
 	private Image _image;
+	private double _fillFactor = 0.9;
+	private double _scaleFactor;
 
 	public ImagePaint(AffineTransform affineTransform, String filename) {
 		super(affineTransform);
@@ -27,7 +28,15 @@ public class ImagePaint extends APaintStrategy {
 	@Override
 	public void init(ASprite context) {
 		_canvas = context.getCanvas();
-		MediaTracker mediaTracker = new MediaTracker(context.getCanvas());
+		MediaTracker mediaTracker = new MediaTracker(context.getCanvas());	// Image must load before use.
+		mediaTracker.addImage(_image, 1);
+		try {
+			mediaTracker.waitForAll();
+		} catch (Exception e) {
+			System.err.println("ImagePaint.init(): Error waiting for image to load. Exception e = " + e + "\n");
+		}
+		
+		_scaleFactor = 2.0 / (_fillFactor * (_image.getWidth(_canvas) + _image.getHeight(_canvas)) / 2.0); 
 	}
 
 	@Override
