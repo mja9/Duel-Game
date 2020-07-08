560232bea047f992f1e64c9a1fb3b6fddca2695b
mja9 <mja9@rice.edu>
2020-07-07 19:00:54 -0500

    added play and settings button to test menu

--- Dastardly Duel/src/view/menu/TitleScreen.java
+++ Dastardly Duel/src/view/menu/TitleScreen.java
@@ -9,6 +9,7 @@ import javax.swing.JLabel;
 import java.awt.Component;
 import java.awt.Dimension;
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
 
 public class TitleScreen extends JPanel {
 
@@ -23,9 +24,11 @@ public class TitleScreen extends JPanel {
 	
 	private final JLabel _lblTitle = new JLabel();
 	
-	private final Image _bgImage = _toolKit.getImage(this.getClass().getResource("images/bg.png"));
+	private final JButton _btnSinglePlayer = new JButton();
 	
-	private final Image _titleImage = _toolKit.getImage(this.getClass().getResource("images/header.png"));
+	private final Image _imgBG = _toolKit.getImage(this.getClass().getResource("images/bg.png"));
+	
+	private final Image _imgTitle = _toolKit.getImage(this.getClass().getResource("images/header.png"));
 
 	/**
 	 * Create the panel. This is where direct one-time manipulation of the panel itself can occur.
@@ -39,15 +42,17 @@ public class TitleScreen extends JPanel {
 		_lblTitle.setBounds(_screenSize.width / 2 - _screenSize.width * 3 / 8, _screenSize.height / 12, 
 				_screenSize.width * 3 / 4, _screenSize.height * 3 / 8);
 		_lblTitle.setAlignmentX(Component.CENTER_ALIGNMENT);
-		Image resizedTitle = _titleImage.getScaledInstance(_lblTitle.getWidth(), _lblTitle.getHeight(), Image.SCALE_DEFAULT);
+		Image resizedTitle = _imgTitle.getScaledInstance(_lblTitle.getWidth(), _lblTitle.getHeight(), Image.SCALE_DEFAULT);
 		_lblTitle.setIcon(new ImageIcon(resizedTitle));
 		add(_lblTitle);
+				
+		// Setting the game buttons
 		
 	}
 	
 	@Override
 	public void paintComponent(Graphics g) {
-		g.drawImage(_bgImage, 0, 0, _screenSize.width, _screenSize.height, this);
+		g.drawImage(_imgBG, 0, 0, _screenSize.width, _screenSize.height, this);
 	}
 
 }
