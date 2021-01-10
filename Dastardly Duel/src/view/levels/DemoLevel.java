72bc185815c35b30a34172e10e3a230bbdec1526
mja9 <mja9@rice.edu>
2021-01-09 20:20:45 -0600

    game demo ready -- how to play added

--- Dastardly Duel/src/view/levels/DemoLevel.java
+++ Dastardly Duel/src/view/levels/DemoLevel.java
@@ -1,14 +1,22 @@
 package view.levels;
 
 import javax.swing.AbstractAction;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextPane;
 import javax.swing.KeyStroke;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.DefaultStyledDocument;
+import javax.swing.text.Document;
+import javax.swing.text.Style;
+import javax.swing.text.StyleContext;
 
 import view.IView2ModelAdapter;
 
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.Rectangle;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.util.function.Consumer;
@@ -29,6 +37,14 @@ public class DemoLevel extends JPanel {
 		_view2Model = view2Model;
 		
 		setBackground(Color.GRAY);	// Set the background.
+		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();	
+		// TODO: Add instructions for the demo here.
+		JLabel instructions = new JLabel();
+		instructions.setBounds(new Rectangle(screenSize.width / 16, screenSize.height - (screenSize.height / 8), screenSize.width / 4, screenSize.height / 8));
+		this.add(instructions);
+		instructions.setText("Move with the arrowkeys, M - Attack, N - Block. Run over the square to fight the boss.");
+				
+
 	}
 	
 	@Override 
@@ -50,7 +66,6 @@ public class DemoLevel extends JPanel {
 		
 		g.setColor(Color.CYAN);
 		g.fillRect(0, screenSize.height - (screenSize.height / 4), screenSize.width, screenSize.height / 4);	// Add a floor
-		// TODO: Add instructions for the demo here.
 		// Move with the arrowkeys, M - Attack, N - Block, 
 		
 		_view2Model.update(g);
